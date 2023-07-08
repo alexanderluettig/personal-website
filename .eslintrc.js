41ceee8860c24eed43290809160d7c0aa66aade2
Alexander Lüttig <alexanderluettig@gmail.com>
2023-07-08 10:03:15 +0200

    Changed Config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,14 @@
 module.exports = {
-  plugins: ['prettier'],
-  extends: ['next/core-web-vitals'],
-  rules: {
-    'no-console': 'error',
-    'prettier/prettier': 'warn',
-    'react-hooks/exhaustive-deps': 'off',
-  },
+    plugins: ['prettier'],
+    extends: ['next/core-web-vitals'],
+    rules: {
+        'no-console': 'error',
+        'prettier/prettier': [
+            'warn',
+            {
+                tabWidth: 4,
+            },
+        ],
+        'react-hooks/exhaustive-deps': 'off',
+    },
 };
